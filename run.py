410f928cfdf13e07b2da359ec7f7d694d3a18693
Kirari04 <lev@saminskij.ch>
2023-04-04 23:16:05 +0200

    added subtitle support

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
+import uuid
 from flask import Flask, request
+import re
+import pysubs2
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
 
+modelPath = './nllb-200-distilled-600M'
 cache = dict()
 app = Flask(__name__)
 
@@ -21,17 +25,51 @@ def post_translate():
     return translate(text, src_lang, tgt_lang)
 
 
+@app.post('/translate/subtitle')
+def post_translate_sub():
+    text = request.form.get('text')
+    subtype = request.form.get('subtype')
+    src_lang = request.form.get('src_lang')
+    tgt_lang = request.form.get('tgt_lang')
+
+    tmpSubFile = "{}.{}".format(uuid.uuid5(uuid.NAMESPACE_URL, text), subtype)
+    f = open(tmpSubFile, "w")
+    f.write(text)
+    f.close()
+
+    subtitles = pysubs2.load(path=tmpSubFile)
+    for subtitle in subtitles:
+        description_pattern = re.compile("^(\[|\()((?!(\]|\))$).*)(\]|\))$")
+        if description_pattern.match(subtitle.text):
+            # is audio description
+            subtitle.text = "[{}]".format(
+                translate(
+                    description_pattern.sub("\\2", subtitle.text),
+                    src_lang,
+                    tgt_lang
+                ),
+            )
+        else:
+            # is caption
+            subtitle.text = translate(subtitle.text, src_lang, tgt_lang)
+    subtitles.save(tmpSubFile)
+
+    f = open(tmpSubFile, "r")
+    returnStr = f.read()
+    f.close()
+    return returnStr
+
+
 def translate(text, src_lang, tgt_lang):
 
     text_hash = hash(text)
     cacheKey = "{}-to-{}__{}".format(src_lang, tgt_lang, text_hash)
 
     if cacheKey not in cache:
-        tokenizer = AutoTokenizer.from_pretrained("./nllb-200-distilled-600M")
-        model = AutoModelForSeq2SeqLM.from_pretrained(
-            "./nllb-200-distilled-600M")
+        tokenizer = AutoTokenizer.from_pretrained(modelPath)
+        model = AutoModelForSeq2SeqLM.from_pretrained(modelPath)
         translator = pipeline('translation', model=model, tokenizer=tokenizer,
-                              src_lang=src_lang, tgt_lang=tgt_lang, max_length=400)
+                              src_lang=src_lang, tgt_lang=tgt_lang,  max_length=400)
         translatedText = translator(text)[0]['translation_text']
         cache[cacheKey] = translatedText
         return translatedText
@@ -41,6 +79,7 @@ def translate(text, src_lang, tgt_lang):
 
 @app.errorhandler(Exception)
 def exception_handler(error):
+    print(error)
     return "Internal Server Error", 500
 
 
